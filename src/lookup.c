89369cec3f502e7c774540484d2490ca145e5a74
Daniel Lovasko <lovasko@freebsd.org>
2015-04-19 01:46:21 +0100

    cache: miss recording

--- src/lookup.c
+++ src/lookup.c
@@ -4,12 +4,13 @@
 #include "lookup.h"
 
 int 
-lookup(char* key, struct l1_entry* l1, struct trie* l2, struct tib_file* tf_out)
+lookup(char* key, struct l1_entry* l1, struct trie* l2, struct tib_file* tf_out, struct list** files, struct trie* miss)
 {
 	unsigned int i;
 	int64_t key_hash;
 	int32_t key_length;
 	struct l1_entry* entry;
+	struct tib_file* miss_file;
 	
 	key_length = strlen(key);
 	key_hash = l1_hash(key, key_length);
@@ -33,6 +34,26 @@ lookup(char* key, struct l1_entry* l1, struct trie* l2, struct tib_file* tf_out)
 	/* L2 */
 	if (trie_get(l2, key, &tf_out) == TRIE_FOUND)
 		return TIBOR_FOUND;
+
+	printf("Logging a miss\n");
+	/* MISS */
+	if (trie_get(miss, key, &miss_file) == TRIE_FOUND)
+		miss_file->misses++;
+	else
+	{
+		struct tib_file* new_miss;
+		new_miss = malloc(sizeof(struct tib_file));
+		new_miss->misses = 1;
+		new_miss->l1_hits = 0;
+		new_miss->l2_hits = 0;
+		memcpy(new_miss->path, key, strlen(key));
+		
+		trie_set(miss, key, new_miss);		
+
+		struct list* new_miss2;
+		new_miss2 = list_create(new_miss);
+		*files = list_add(*files, new_miss2);
+	}
 
 	return TIBOR_NOT_FOUND;
 }
--- src/lookup.h
+++ src/lookup.h
@@ -4,12 +4,13 @@
 #include "file.h"
 #include "l1.h"
 #include "trie.h"
+#include "list.h"
 
 #define TIBOR_FOUND 1
 #define TIBOR_NOT_FOUND 2
 
 int 
-lookup(char* key, struct l1_entry* l1, struct trie* l2, struct tib_file* tf_out);
+lookup(char* key, struct l1_entry* l1, struct trie* l2, struct tib_file* tf_out, struct list** files, struct trie* miss);
 
 #endif
 
