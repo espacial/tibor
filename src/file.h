0ea4527d9c8dee29ba463b517bb0c3853f3b9652
Daniel Lovasko <lovasko@freebsd.org>
2015-04-19 01:17:56 +0100

    stats: initial data structure members

--- src/file.h
+++ src/file.h
@@ -1,11 +1,21 @@
 #ifndef TIB_FILE_H
 #define TIB_FILE_H
 
+#include <stdint.h>
+
 struct tib_file
 {
+	char path[1024];
 	int fd;
+
 	int shmid;
 	uint64_t offset;
+
+	uint8_t always_l1;
+
+	uint64_t l1_hits;
+	uint64_t l2_hits;
+	uint64_t misses;
 };
 
 #endif
